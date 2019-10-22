8678d71c96d89bb2b9a529fcfc3f5b90ccf75a46
Silvern29 <48048266+Silvern29@users.noreply.github.com>
2019-10-22 15:32:00 +0200

    up

---
+++ src/at/redlinghaus/Game.java
@@ -0,0 +1,4 @@
+package at.redlinghaus;
+
+public class Game {
+}
--- src/at/redlinghaus/Main.java
+++ src/at/redlinghaus/Main.java
@@ -3,6 +3,6 @@ package at.redlinghaus;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
